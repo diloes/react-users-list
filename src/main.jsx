3a7c858dcef03b2696c3fc5e0295be21de653bf5
Diloes <diloes@hotmail.com>
2022-06-26 19:50:32 +0200

    lista usuarios creada

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,26 @@
-import ReactDOM from 'react-dom';
-import './index.css';
+import ReactDOM from 'react-dom'
+import './index.css'
 
-const app = <img src='/logo.svg' />;
-const container = document.getElementById('root');
+const Title = () => <h1>Listado de Usuarios</h1>
 
-ReactDOM.render(app, container);
+const User = ({ name, active, role }) => {
+  return (
+    <div className='user'>
+      <span className='name' >{name}</span>
+      <span className='active' >{active}</span>
+      <span className='role' >{role}</span>
+    </div>
+  )
+}
+
+const app = (
+	<div className='list'>
+		<Title />
+    <User name='Pablo Castellanos' active='activo' role='Profesor' />
+    <User name='Diego López' active='activo' role='Alumno' />
+	</div>
+)
+
+const container = document.getElementById('root')
+
+ReactDOM.render(app, container)
