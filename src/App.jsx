6179bf906ba40c23872d924c0c3b54d756280c16
diloes <diloes@hotmail.com>
2022-07-03 21:29:11 +0200

    useEffect y sus dependencias

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
-import UsersList from './components/UsersList'
+import { useEffect, useState } from 'react'
 
-const USERS = [
-	{
-		id: 0,
-		name: 'Pablo Castellanos',
-		active: true,
-		role: 'teacher'
-	},
-	{
-		id: 1,
-		name: 'Diego López',
-		active: true,
-		role: 'student'
-	},
-	{
-		id: 2,
-		name: 'Rocío Ortiz',
-		active: false,
-		role: 'student'
-	}
-]
+const App = () => {
+	const [count, setCount] = useState(0)
+	const [step, setStep] = useState(1)
 
-const App = () => <UsersList initialUsers={USERS} />
+	console.log('Render Antes de useEffect')
+
+	useEffect(() => {
+		document.title = count
+		console.log('useEffect')
+	}, [count])
+
+	console.log('Render después de useEfect')
+
+	return (
+		<div>
+			<h1>{count}</h1>
+			<h2>{step}</h2>
+			<button onClick={() => setCount(count + step)}>+{step}</button>
+			<button onClick={() => setStep(step + 1)}>Incrementar step</button>
+		</div>
+	)
+}
 
 export default App
