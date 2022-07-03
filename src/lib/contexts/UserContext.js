2706e94f95d2391bd2f3145ae465964da2715a75
diloes <diloes@hotmail.com>
2022-07-03 15:39:40 +0200

    context

--- src/lib/contexts/UserContext.js
+++ src/lib/contexts/UserContext.js
@@ -1,4 +1,4 @@
 import { createContext } from 'react'
 
 // Creamos el context
-export const UsersContext = createContext()
+export const UsersContext = createContext('Valor por defecto sin sentido que no debemos poner')
